e90d806b93c175b32b68fd6e2923bfccadda300d
fabioluccioletti <luccioletti.fabio@gmail.com>
2019-06-30 15:45:38 +0200

    Initial implementation of Game Class

--- Game.cpp
+++ Game.cpp
@@ -3,3 +3,75 @@
 //
 
 #include "Game.h"
+
+Game::Game() : window(sf::VideoMode(600, 600), "A Space Game"), isPaused(false),
+               isMovingLeft(false), isMovingRight(false),
+               view((sf::FloatRect(0, 0, window.getSize().x, window.getSize().y))) {
+
+    //Player's spaceship creation
+
+    //Background creation
+
+    //Limitation of the framerate
+    window.setFramerateLimit(60);
+}
+
+void Game::run() {
+    sf::Clock clock;
+    sf::Time deltaTime;
+    while (window.isOpen()) {
+        deltaTime = clock.restart();
+        processEvents();
+        if (!isPaused)
+            update(deltaTime);
+        render();
+
+    }
+
+}
+
+void Game::processEvents() {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+        switch (event.type) {
+            case sf::Event::Closed:
+                window.close();
+                break;
+            case sf::Event::KeyPressed:
+                handlePlayerInput(event.key.code, true);
+                break;
+            case sf::Event::KeyReleased:
+                handlePlayerInput(event.key.code, false);
+                break;
+            case sf::Event::GainedFocus:
+                isPaused = false;
+                break;
+            case sf::Event::LostFocus:
+                isPaused = true;
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+void Game::update(sf::Time dt) {
+    //Player movement
+
+    //View updating
+    view.setCenter(window.getPosition().x - 85, window.getPosition().y + 250);
+    window.setView(view);
+}
+
+void Game::render() {
+    window.clear(sf::Color::Black);
+
+    window.display();
+}
+
+void Game::handlePlayerInput(sf::Keyboard::Key key, bool isPressed) {
+    if (key == sf::Keyboard::Left)
+        isMovingLeft = isPressed;
+    else if (key == sf::Keyboard::Right)
+        isMovingRight = isPressed;
+}
--- Game.h
+++ Game.h
@@ -5,8 +5,33 @@
 #ifndef ASPACEGAME_GAME_H
 #define ASPACEGAME_GAME_H
 
+#include <exception>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
 
 class Game {
+public:
+    Game();
+
+    void run();
+
+
+private:
+    void processEvents();
+
+    void update(sf::Time dt);
+
+    void render();
+
+    void handlePlayerInput(sf::Keyboard::Key key, bool isPressed);
+
+    void backScroll();
+
+    sf::RenderWindow window;
+    sf::View view;
+    bool isPaused;
+    bool isMovingLeft;
+    bool isMovingRight;
 
 };
 
